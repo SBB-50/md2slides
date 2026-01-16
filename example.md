616f9e797343e69d13f91d7845be9bc0dd38acff
SBB-50 <stefanborgar@gmail.com>
2026-01-16 16:20:26 +0000

    first draft

---
+++ example.md
@@ -0,0 +1,15 @@
+# this is a heading
+- bullet point
+*italic*
+**bold**
+# slide 2
+- many
+- bullet
+- points
+- about
+- five (one line) bullet points can fit in one slide, but of course longer lines in the markdown file can change this.
+# lorem ipsum
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur nisl mauris, ut viverra lectus convallis quis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent consequat eros at dapibus dignissim. Integer suscipit rutrum lorem sed blandit. Aenean tempus lectus eget aliquet volutpat. Nulla efficitur semper turpis, nec malesuada nisi iaculis eu. Nunc non orci sit amet tortor rutrum placerat. Quisque vestibulum tortor ex, at vulputate eros vulputate vitae. Phasellus cursus commodo tortor et cursus. Nullam accumsan eu tortor vitae porta. Donec sagittis elit et elit vestibulum finibus. Vestibulum risus magna, aliquam ut justo nec, eleifend porttitor dolor. Pellentesque hendrerit nisl ac turpis blandit, quis tempus magna pulvinar. Suspendisse sed orci urna.
+# how much can you write in one line?
+there is of course a limit on how much space one line can take up in one slide, but another slide is automatically made if the text goes out of bounds.
+you have roughly 28 characters of space in the title until the title goes to the second line and 39 for normal lines
--- main.py
+++ main.py
@@ -1,32 +1,56 @@
 from fpdf import FPDF
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        file_path = sys.argv[1]
+        file_output_name=sys.argv[2]
+    else:
+        if len(sys.argv) > 1:
+            file_path = sys.argv[1]
+            print("output file name not given, outputting to output.pdf")
+            file_output_name="output.pdf"
+        else:
+            print("no input file, exiting")
+            quit()
 
 pdf = FPDF(orientation="P",unit="pt",format=(1200,900))
 pdf.set_top_margin(100)
 pdf.set_right_margin(100)
+pdf.add_font("uHelvetica", style="", fname="Helvetica.ttf")
+pdf.add_font("uHelvetica", style="b", fname="Helvetica-Bold.ttf")
+pdf.add_font("uHelvetica", style="i", fname="Helvetica-Oblique.ttf")
+pdf.add_font("uHelvetica", style="bi", fname="Helvetica-BoldOblique.ttf")
 
+#body text function
 def text(text):
-    pdf.set_font('Helvetica', size=48)
+    pdf.set_font('uHelvetica', size=48)
     pdf.set_left_margin(200)
     pdf.multi_cell(0,text=text, markdown=True)
     pdf.ln(75)
+#title function
 def title(text):
-    pdf.set_font('Helvetica', size=98)
+    pdf.set_font('uHelvetica', size=98)
     pdf.set_left_margin(50)
-    pdf.cell(40,10,text)
-    pdf.ln(100)
+    pdf.multi_cell(0,text=text,align="L")
+    pdf.ln(0)
+#markdown translation function
 def mdtrans(markdown):
     step1 = markdown.replace("*","__")
     step2 = step1.replace("____","**")
-    return step2
-
-pdf.add_page()
-title("header example")
-swag1 = mdtrans("*body text example*")
-swag2 = mdtrans("**body text example**")
-text(swag1)
-text(swag2)
-pdf.add_page()
-title("page 2 header example")
-text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-text("body text example")
-pdf.output("hello_world.pdf")
+    step3 = step2.replace("-","•")
+    return step3
+try: 
+    with open(file_path) as file:
+        content = file.readlines()
+    for i in range (len(content)):
+        if (content[i][0] == '#'):
+            pdf.add_page()
+            title(content[i][1:])
+        else:
+            text(mdtrans(content[i]).removesuffix("\n"))
+except FileNotFoundError:
+    print("file not found")
+except Exception as e:
+    print (f'an error occurred: {e}')
+pdf.output(file_output_name)
